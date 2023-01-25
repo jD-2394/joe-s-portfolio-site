d1619513db752d3f46c98f603d35dada12ecd7c9
Joey Doye <jadoye@gmail.com>
2023-01-24 23:26:28 -0500

    initial commit 2

---
+++ src/components/navComponent.js
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText
+ } from '../styleSheets/layout.module.css'
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About.Me</Link></li>
+          <li className={navLinkItem}><Link to="/experience" className={navLinkText}>Experience</Link></li>
+          <li className={navLinkItem}><Link to="/news" className={navLinkText}>News</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
